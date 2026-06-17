67e0a3fb9201b54ebb8749616758ecbd1ad03530
Vinisha-Veluri <vinishaveluri@gmail.com>
2026-06-17 21:46:05 +0530

    Create code.v

---
+++ 01_Combinational_circuits/14_BCD_Adder/code.v
@@ -0,0 +1,19 @@
+module BCD_Adder(
+input [3:0]a,b,
+input cin,
+output reg [3:0]out,
+output reg cout
+    );
+    reg [4:0]temp;
+    always @(*) begin
+        temp=a+b+cin;
+        if(temp>5'd9) begin
+            temp=temp+5'd6;
+            cout=1'b1;
+            end
+        else
+            cout=1'b0;
+        out[3:0]=temp[3:0];
+        end 
+        
+ endmodule
